5958ccca9de9382ab9dfe62c5ff7e318fed4ed11
ArmineG <arminegevorgyan99@gmail.com>
2019-03-06 21:35:03 +0400

    average of 4 numbers

---
+++ average/average/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input a..");
+            string a = Console.ReadLine();
+            Console.WriteLine("Input b..");
+            string b = Console.ReadLine();
+            Console.WriteLine("Input c..");
+            string c = Console.ReadLine();
+            Console.WriteLine("Input d..");
+            string d = Console.ReadLine();
+
+
+            double a1 = double.Parse(a);
+            double b1 = double.Parse(b);
+            double c1 = double.Parse(c);
+            double d1 = double.Parse(d);
+            double z = (a1 + b1 + c1 + d1) / 4;
+            Console.WriteLine(z);
+            Console.ReadLine();
+        }
+    }
+}
