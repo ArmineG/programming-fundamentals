617b3d80023498c2113104b0928906cfac51044f
ArmineG <arminegevorgyan99@gmail.com>
2019-03-08 19:08:41 +0400

    if one number = 1 true

---
+++ 1/1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a..");
+            string a = Console.ReadLine();
+            Console.WriteLine("Input b..");
+            string b = Console.ReadLine();
+            Console.WriteLine("Input c..");
+            string c = Console.ReadLine();
+            double a1 = double.Parse(a);
+            double b1 = double.Parse(b);
+            double c1 = double.Parse(c);
+            if (a1 == 1 || b1 == 1 || c1 == 1)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ project/project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine("***************Wellcome to my rocking app*********************");
+            Console.WriteLine("**************************************************************");
+           
+
+            
+          
+            Console.Read();
+
+
+           
+
+        }
+    }
+}
