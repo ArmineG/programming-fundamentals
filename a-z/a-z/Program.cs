ffe28260f289ab0989563ae5661cb75beeab2cfc
ArmineG <arminegevorgyan99@gmail.com>
2019-03-08 19:40:15 +0400

    2 numbers = 3

---
+++ a-z/a-z/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a_z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a..");
+            string a = Console.ReadLine();
+            Console.WriteLine("Input b..");
+            string b = Console.ReadLine();
+            Console.WriteLine("Input c..");
+            string c = Console.ReadLine();
+            double a1 = double.Parse(a);
+            double b1 = double.Parse(b);
+            double c1 = double.Parse(c);
+         
+
+            if (a1 == 3 && b1 == 3)
+            {
+                Console.WriteLine(true);
+            }
+            else if (b1 == 3 && c1 == 3)
+            {
+                Console.WriteLine(true);
+            }
+            else if (a1 == 3 && c1 ==3)
+            {
+                Console.WriteLine(true);
+
+            }
+           else
+            {
+                Console.WriteLine(false);
+            }
+            Console.Read();
+        }
+    }
+}
