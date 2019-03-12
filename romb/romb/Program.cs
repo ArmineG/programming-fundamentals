1347178e66ff311e0d75e2488f75c6aa0168716b
ArmineG <arminegevorgyan99@gmail.com>
2019-03-13 01:23:53 +0400

    Dimond

---
+++ romb/romb/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace romb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int row = int.Parse(a);
+
+            for (int i = 1; i <= row; i++)
+
+            {
+
+                for (int j = 0; j < (row - i); j++)
+
+                    Console.Write(" ");
+
+                for (int j = 1; j <= i; j++)
+
+                    Console.Write("*");
+
+                for (int k = 1; k < i; k++)
+
+                    Console.Write("*");
+
+                Console.WriteLine();
+
+            }
+
+
+
+            for (int i = row - 1; i >= 1; i--)
+
+            {
+
+                for (int j = 0; j < (row - i); j++)
+
+                    Console.Write(" ");
+
+                for (int j = 1; j <= i; j++)
+
+                    Console.Write("*");
+
+                for (int k = 1; k < i; k++)
+
+                    Console.Write("*");
+
+                Console.WriteLine();
+
+            }
+
+
+
+            Console.WriteLine();
+            Console.Read();
+
+        }
+
+    }
+}
+
