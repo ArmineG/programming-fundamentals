f7ad293081d06a07eca7ad859d83c6f0d9ef4a47
ArmineG <arminegevorgyan99@gmail.com>
2019-03-13 00:15:48 +0400

    sum of odd numbers

---
+++ 1-n odd sum/1-n odd sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_n_odd_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int n = int.Parse(number);
+            int S = 0;
+                      
+            for (int i = 1; i <= n; i+=2)
+            {
+                S += i;
+
+                
+                
+            }
+            Console.WriteLine(S);
+
+
+            Console.Read();
+
+        }
+    }
+}
