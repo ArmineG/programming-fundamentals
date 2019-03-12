64d0cdbde2890e1b11a9f15846469d22b61e107a
ArmineG <arminegevorgyan99@gmail.com>
2019-03-13 00:18:15 +0400

    Sum of numbers till n

---
+++ Sum1/Sum1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int n = int.Parse(number);
+            int S = 0;
+
+            for (int i = 1; i <= n; i ++)
+            {
+                S += i;
+
+
+
+            }
+            Console.WriteLine(S);
+
+
+            Console.Read();
+        }
+    }
+}
