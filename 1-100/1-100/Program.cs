18deded96edeedfecdf6b2ac49fa18f4414d0a75
ArmineG <arminegevorgyan99@gmail.com>
2019-03-12 21:48:05 +0400

    1-100

---
+++ 1-100/1-100/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            int i = 0;
+           
+            while(i<n)
+            {
+                i= i + 2;
+                Console.WriteLine(i);
+            }
+
+            Console.Read();
+        }
+    }
+}
