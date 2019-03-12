d21a0dc8db9299e347bad1710a6f36190a98c5e0
ArmineG <arminegevorgyan99@gmail.com>
2019-03-12 21:25:34 +0400

    1-n

---
+++ n-1/n-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace n_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            int n = int.Parse(number);
+            int i = 0;
+            while (i < n)
+            {
+                i = i + 1 ;
+                Console.WriteLine(i);
+            }
+          
+            Console.Read();
+        }
+    }
+}
